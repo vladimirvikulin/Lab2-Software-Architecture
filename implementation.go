c972e79455d729c2baa7f8aa47056ff9306bb0c7
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-03-10 22:08:56 +0200

    fixed bugs / added errors check

--- implementation.go
+++ implementation.go
@@ -1,7 +1,7 @@
-package calculator
+package lab2
 
 import (
-	"fmt"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -14,7 +14,7 @@ func Evaluate(expression string) (float64, error) {
 		switch token {
 		case "+", "-", "*", "/", "^":
 			if len(stack) < 2 {
-				return 0, fmt.Errorf("invalid expression")
+				return 0, InvalidExpressionError{}
 			}
 			var result float64
 			operand2 := stack[len(stack)-1]
@@ -25,11 +25,13 @@ func Evaluate(expression string) (float64, error) {
 				result = operand1 + operand2
 			case "-":
 				result = operand1 - operand2
+			case "^":
+				result = math.Pow(operand1, operand2)
 			case "*":
 				result = operand1 * operand2
 			case "/":
 				if operand2 == 0 {
-					return 0, fmt.Errorf("division by zero")
+					return 0, DivisionByZeroError{}
 				}
 				result = operand1 / operand2
 			}
@@ -37,10 +39,25 @@ func Evaluate(expression string) (float64, error) {
 		default:
 			operand, err := strconv.ParseFloat(token, 64)
 			if err != nil {
-				return 0, fmt.Errorf("invalid expression")
+				return 0, InvalidExpressionError{}
 			}
 			stack = append(stack, operand)
 		}
 	}
+	if len(stack) != 1 {
+		return 0, InvalidExpressionError{}
+	}
 	return stack[0], nil
+}
+
+type InvalidExpressionError struct{}
+
+func (e InvalidExpressionError) Error() string {
+	return "invalid expression"
+}
+
+type DivisionByZeroError struct{}
+
+func (e DivisionByZeroError) Error() string {
+	return "division by zero"
 }
