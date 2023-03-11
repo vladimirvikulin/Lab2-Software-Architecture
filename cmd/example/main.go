f4ac4ae6e76b3fb30820db55b57a409c5c71b3a3
LLumisS <vasa16671@gmail.com>
2023-03-11 23:59:02 +0200

    added main func

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -3,25 +3,59 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	lab2 "github.com/vladimirvikulin/Lab2-Software-Architecture"
 )
 
 var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
+	exprPtr    = flag.String("e", "", "Expression to evaluate")
+	filePtr    = flag.String("f", "", "File containing expression to evaluate")
+	outFilePtr = flag.String("o", "", "File to output result")
 )
 
 func main() {
 	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	var reader io.Reader
+	if *filePtr != "" {
+		file, err := os.Open(*filePtr)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error opening file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		reader = file
+	} else if *exprPtr != "" {
+		reader = strings.NewReader(*exprPtr)
+	} else {
+		fmt.Fprintln(os.Stderr, "Either -e or -f flag must be provided")
+		os.Exit(1)
+	}
 
-	res, _ := lab2.EvaluatePostfix("2 2 +")
-	fmt.Println(res)
+	var writer io.Writer
+	if *outFilePtr != "" {
+		file, err := os.Create(*outFilePtr)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error creating file:", err)
+			os.Exit(1)
+		}
+		defer file.Close()
+		writer = file
+	} else {
+		writer = os.Stdout
+	}
+
+	computeHandler := &lab2.ComputeHandler{
+		Input:  reader,
+		Output: writer,
+	}
+
+	err := computeHandler.Compute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error computing expression:", err)
+		os.Exit(1)
+	}
 }
